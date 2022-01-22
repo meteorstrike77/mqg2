2256986bfb8e3732509a968e2f1b34260d1987bd
meteorstrike77 <89230784+meteorstrike77@users.noreply.github.com>
2022-01-22 11:37:42 -0500

    Add files via upload

--- quizgamelogin.js
+++ quizgamelogin.js
@@ -1,9 +1,9 @@
 function adduser() {
-    player1name = document.getElementById("player1_name_input").value;
-    player2name = document.getElementById("player2_name_input").value;
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
 
-        localStorage.setItem("player1name", player1name);
-        localStorage.setItem("player2name", player2name);
+        localStorage.setItem("player1name", player1_name);
+        localStorage.setItem("player2name", player2_name);
 
 window.location = "quizgamepage.html";
 }
